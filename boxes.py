a59540b478fb9dc66a8cf3c3eb17f5caf3261844
John Sturdy <jcg.sturdy@gmail.com>
2021-09-22 20:07:47 +0100

    start adding holes and custom types

--- boxes.py
+++ boxes.py
@@ -8,6 +8,10 @@ wall_thickness = %g;
 floor_thickness = %g;
 ceiling_thickness = %g;
 
+module hole(preshift, rot, postshift, dimensions) {
+    translate(preshift) rotate(rot) translate(postshift) cube(dimensions);
+}
+
 module box(position, dimensions, opacity, label) {
     color([.5, .5, .5, .5]) {
         translate(position) {
@@ -17,12 +21,28 @@ module box(position, dimensions, opacity, label) {
                 translate([wall_thickness/2, wall_thickness/2, floor_thickness]) {
                     cube([dimensions[0]-wall_thickness, dimensions[1]-wall_thickness, dimensions[2]-floor_thickness]);
                 }
+                children();
             }
         }
     }
 }
 """
 POSTAMBLE = """"""
+
+# scratch = """
+# box([0.0, 0.0, 0.0], [435.0, 385.0, 253.0], 1, "Living room") {
+#      wallrotate = 90;
+#      fromwall = 90;
+#      fromfloor = 70;
+#      width = 245;
+#      height = 135;
+#      rotate([0, 0, wallrotate]) {
+#           translate([fromwall, -10, fromfloor]) {
+#           cube([width, 20, height]);
+#           }
+#      }
+# };
+# """
 
 class Box(object):
 
@@ -33,7 +53,7 @@ class Box(object):
 
     def __init__(self, data):
         self.box_type = data.get('type', 'room')
-        self.location = data.get('location')
+        self.name = data.get('name')
         self.dimensions = [float(data.get('width')),
                            float(data.get('depth')),
                            float(data.get('height'))]
@@ -41,21 +61,29 @@ class Box(object):
         self.adjacent = data.get('adjacent')
         self.direction = data.get('direction')
         self.alignment = data.get('alignment')
-        self.offset = data.get('offset', 0.0)
+        self.offset = data.get('offset', 0.0) or 0.0
+        self.holes = []
         self.opacity = 1.0
 
     def __str__(self):
         return "<box %s of size %s at %s to %s of %s, %s-aligned>" % (
-            self.location,
+            self.name,
             self.dimensions,
             self.position,
             self.direction, self.adjacent, self.alignment)
 
     def write_scad(self, stream):
-        stream.write("""box(%s, %s, %g, "%s");\n""" % (
+        stream.write("""box(%s, %s, %g, "%s") {\n""" % (
             self.position,
             self.dimensions,
-            self.opacity, self.location))
+            self.opacity, self.name))
+        for hole in self.holes:
+            hole.write_scad(stream)
+        stream.write("""};\n""")
+
+def cell_as_float(row, name):
+    value = row.get(name)
+    return 0 if value in (None, "") else float(value)
 
 class Hole(object):
 
@@ -65,12 +93,25 @@ class Hole(object):
     pass
 
     def __init__(self, data):
-        self.dimensions = [float(data.get('width')),
-                           float(data.get('depth')), # from bottom to top of the hole
-                           float(data.get('height'))] # from the floor to the bottom of the hole
-        self.cut_out_from = data.get('adjacent')      # the room that this hole is in one of the walls of
+        self.name = data['name']
+        self.dimensions = [float(data.get('width')),  # from one side of the hole to the other
+                           float(data.get('depth')),  # from bottom to top of the hole
+                           0]                         # fill in the thickness of the hole later
+        self.adjacent = data.get('adjacent')          # the room that this hole is in one of the walls of
         self.direction = data.get('direction')        # which wall the hole is in (front, left, back, right)
-        self.offset = data.get('offset', 0.0)
+        self.height = cell_as_float(data, 'height')   # from the floor to the bottom of the hole
+        self.offset = cell_as_float(data, 'offset')   # how far from the start of the wall the hole starts
+        self.preshift = [0, 0, 0]                     # translate to the start of the wall the hole is in
+        self.rotate = 0                               # rotate to match the wall
+        self.postshift = [0, 0, 0]                    # translate to the right place on the wall
+
+    def write_scad(self, stream):
+        stream.write("""    hole(%s, %s, %s, %s, "%s");\n""" % (
+            self.preshift,
+            self.rotate,
+            self.postshift,
+            self.dimensions,
+            self.name))
 
 class Constant(object):
 
@@ -79,9 +120,31 @@ class Constant(object):
     pass
 
     def __init__(self, data):
-        self.name = data['location']
+        self.name = data['name']
         self.value = data['width']
         self.all_data = data
+
+class Type(object):
+
+    """A type definition.
+
+    Not implemented yet."""
+
+    pass
+
+    def __init__(self, data):
+        self.data = data
+        self.name = data['name']
+        makers[self.name] = makers['__custom__']
+
+class Custom(object):
+
+    """A custom type, as defined by the Type type of row."""
+
+    pass
+
+    def __init__(self, data):
+        self.data = data
 
 # Define all the names for each of the functions to make an object
 # from a spreadsheet row (since there are multiple names for each
@@ -90,7 +153,9 @@ class Constant(object):
 names_for_makers = {
     lambda row: Box(row): ('room', 'shelf', 'shelves', 'box'),
     lambda row: Hole(row): ('door', 'window'),
-    lambda row: Constant(row): ('constant',)}
+    lambda row: Constant(row): ('constant',),
+    lambda row: Type(row): ('type',),
+    lambda row: Custom(row): ('__custom__',)}
 
 # Invert the table, so we can look up row types in it:
 makers = {
@@ -99,8 +164,8 @@ makers = {
     for name in names}
 
 def process_with_dependents(boxes, dependents, box, level):
-    if box.location in dependents:
-        for dependent_name in dependents[box.location]:
+    if box.name in dependents:
+        for dependent_name in dependents[box.name]:
             dependent = boxes[dependent_name]
 
             if isinstance(dependent, Box):
@@ -135,7 +200,7 @@ DEFAULT_CONSTANTS = {
 def add_default_constants(boxes):
     for default_name, default_value in DEFAULT_CONSTANTS.items():
         if default_name not in boxes:
-            boxes[default_name] = Constant({'location': default_name, 'width': default_value})
+            boxes[default_name] = Constant({'name': default_name, 'width': default_value})
 
 def main():
     parser = argparse.ArgumentParser()
@@ -144,20 +209,22 @@ def main():
     args = parser.parse_args()
 
     with open(args.inputfile) as instream:
-        boxes = {row['location']: makers[row.get('type', 'room')](row)
+        boxes = {row['name']: makers[row.get('type', 'room')](row)
                        for row in csv.DictReader(instream)}
 
     # Work out the tree structure of what depends on what:
     dependents = {}
     for box in boxes.values():
+        if not (isinstance(box, Box) or isinstance(box, Hole)):
+            continue
         adjacent = box.adjacent
         if adjacent == 'start':
-            dependents['start'] = [box.location]
+            dependents['start'] = [box.name]
             first_box = box
         else:
             if adjacent not in dependents:
                 dependents[adjacent] = []
-            dependents[adjacent].append(box.location)
+            dependents[adjacent].append(box.name)
     if 'start' not in dependents:
         print("No starting point given")
 
@@ -173,6 +240,8 @@ def main():
             box.dimensions[0] += wall_thickness # one half-thickness at each side
             box.dimensions[1] += wall_thickness # one half-thickness at each end
             box.dimensions[2] += floor_thickness + ceiling_thickness
+        elif isinstance(box, Hole):
+            box.dimensions[2] = wall_thickness * 2
 
     # Now process the tree
     first_box.position = [0.0, 0.0, 0.0]
