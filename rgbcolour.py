e817428ba75bcd9816c01591add6c3d2cf41af16
John C. G. Sturdy <jcg.sturdy@gmail.com>
2022-12-24 11:09:32 +0000

    Add RGB colour conversion in preparation for adding transparency control

---
+++ rgbcolour.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# manipulate box colours
+
+def read_colour_file():
+    """Return a dictionary mapping colour names to RGB channels in range 0..1."""
+    with open("/etc/X11/rgb.txt") as instream:
+        return {
+            splitline[1].strip(): [float(elt)/255.0
+                                   for elt in splitline[0].split()]
+            for splitline in [line.split("\t\t")
+                              for line in instream.readlines()[1:]]}
+
+COLOUR_TABLE = None
+
+def rgbcolour(name):
+    global COLOUR_TABLE
+    if not COLOUR_TABLE:
+        COLOUR_TABLE = read_colour_file()
+    return COLOUR_TABLE[name]
+
+# for testing:
+if __name__ == "__main__":
+    for name, value in read_colour_file().items():
+        print(name, "is", value)
